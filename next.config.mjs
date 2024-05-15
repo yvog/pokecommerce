52c684482c0731e527b5b212d8745b42b8f75459
Yvo Geldhof <18664669+yvog@users.noreply.github.com>
2024-05-15 18:52:41 +0200

    chore: add images remote patterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'unpkg.com/pokeapi-sprites@2.0.2',
+        port: '',
+        pathname: '/sprites/pokemon/other/dream-world/**',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
