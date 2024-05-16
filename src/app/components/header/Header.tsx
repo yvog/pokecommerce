98a276d7b344cf2ed9745a758a3302a7ea59006a
Yvo Geldhof <18664669+yvog@users.noreply.github.com>
2024-05-16 21:00:31 +0200

    feat: add header component

---
+++ src/app/components/header/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+type HeaderProps = {
+  title: React.ReactNode;
+};
+
+export const Header = ({ title }: HeaderProps) => {
+  return (
+    <header className="flex flex-col items-center justify-between p-24">
+      <h1 className="text-4xl font-bold sm:text-6xl sm:leading-tight bg-gradient-to-r from-primary to-primaryLight text-transparent bg-clip-text">
+        {title}
+      </h1>
+    </header>
+  );
+};
